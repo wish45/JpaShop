1ba7560a763626f883e2f32db26439a890a6a3c5
newhyodong <hyodg90@gmail.com>
2023-04-10 12:47:47 +0900

    Initial commit

---
+++ src/main/java/jpabook/jpashop/exception/NotEnoughStockException.java
@@ -0,0 +1,23 @@
+package jpabook.jpashop.exception;
+
+import com.zaxxer.hikari.SQLExceptionOverride;
+
+public class NotEnoughStockException extends RuntimeException{
+    public NotEnoughStockException() {
+        super();
+    }
+
+    public NotEnoughStockException(String message) {
+        super(message);
+    }
+
+    public NotEnoughStockException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public NotEnoughStockException(Throwable cause) {
+        super(cause);
+    }
+
+
+}
---
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,37 @@
+package jpabook.jpashop;
+
+import jpabook.jpashop.repository.MemberRepository;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MemberRepositoryTest {
+    @Autowired
+    MemberRepository memberRepository;
+
+
+
+  /*  @Test
+    @Transactional
+    @Rollback(false)
+    public void testMember() throws Exception{
+        //given
+        Member member = new Member();
+        member.setUsername("memberA");
+
+        //when
+        Long saveId = memberRepository.save(member);
+        Member findMember = memberRepository.find(saveId);
+
+        //then
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        Assertions.assertThat(findMember).isEqualTo(member);  //true가 나온다. 영속성 컨텍스트에서 1차캐시에 있는것을 조회하기때문에
+        
+
+    }*/
+            
+}
