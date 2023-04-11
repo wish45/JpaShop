f7824d103fc2cb2d0f20d7118e47d8bca76ea2aa
wish4 <wish4@hansse.com>
2023-04-11 14:34:19 +0900

    #엔티티를 DTO로 변환 - 페이징과 한계 돌파

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -33,10 +33,10 @@ public class OrderRepository {
 
     public List<Order> findAllWithMemberDelivery() {
         return em.createQuery("select o " +
-                        "from Order o " +
-                        "join fetch o.member m "
-                        + "join fetch o.delivery d", Order.class)
-                .getResultList();
+                "from Order o " +
+                "join fetch o.member m "
+                + "join fetch o.delivery d", Order.class)
+            .getResultList();
     }
 
     public List<Order> findAllWithItem() {
@@ -46,7 +46,9 @@ public class OrderRepository {
                 "join fetch o.delivery d"+
                 "join fetch o.orderItems oi"+
                 "join fetch oi.item i", Order.class)
-                .getResultList();
+            .setFirstResult(1)
+            .setMaxResults(100)
+            .getResultList();
     }
 
 
@@ -54,5 +56,13 @@ public class OrderRepository {
 /*    public List<Order> findAllByCriteria(OrderSearch orderSearch){
         
     }*/
-
+    public List<Order> findAllWithMemberDelivery(int offset, int limit) {
+        return em.createQuery("select o " +
+                "from Order o " +
+                "join fetch o.member m "
+                + "join fetch o.delivery d", Order.class)
+            .setFirstResult(offset)
+            .setMaxResults(limit)
+            .getResultList();
+    }
 }
