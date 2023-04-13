5f4ff8c5a7c0b766c96ba48b4ab18157cfa9fea7
wish4 <wish4@hansse.com>
2023-04-13 14:13:19 +0900

    #스프링 데이터 JPA 소개

--- src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -1,37 +1,10 @@
 package jpabook.jpashop.repository;
 
-import jpabook.jpashop.domain.Member;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Repository;
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import java.util.List;
+import jpabook.jpashop.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-@Repository
-@RequiredArgsConstructor
-public class MemberRepository {
+public interface MemberRepository extends JpaRepository<Member, Long> {
 
-    private final EntityManager em;
-    
-    //@PersistenceContext //엔티티매니저를 주입해준다
-    //private EntityManager em;
-
-    public void save(Member member){
-        em.persist(member);
-    }
-    public Member findOne(Long id){
-        return em.find(Member.class,id);
-    }
-
-    public List<Member> findAll(){
-        return em.createQuery("select m from Member m", Member.class)
-                .getResultList();
-    }
-
-    public List<Member> findByName(String name){
-        return em.createQuery("select m from Member m where m.name=:name", Member.class)
-                .setParameter("name",name)
-                .getResultList();
-    }
+    List<Member> findByName(String name);
 }
